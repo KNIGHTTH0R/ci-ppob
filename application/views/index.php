ab32f2cf92c70d6ce958fdf1ce4370417acdf364
mukhoiran <mkhoir.anam@gmail.com>
2019-04-15 13:51:18 +0700

    fix tab listrik

--- application/views/index.php
+++ application/views/index.php
@@ -111,24 +111,24 @@
         </div>
       </div>
     </div>
-    <!-- <div role="tabpanel" class="tab-pane" id="listrik">
+    <div role="tabpanel" class="tab-pane" id="listrik">
       <div class="card">
         <div class="card-header">
           Isi Listrik Saya
         </div>
         <div class="card-body">
-          <form action="<?=base_url('home/topup_request/')?>" id="ppob" method="post">
+          <form action="<?=base_url('home/topup_request/')?>" id="ppob-listrik" method="post">
             <div class="row">
-                <input type="hidden" name="order_type" id="order_type" value="pln">
+                <input type="hidden" name="order_type" id="order_type_listrik" value="pln">
                 <div class="col-md-4">
                   <div class="form-group">
-                   <input type="text" class="form-control" id="nomor" name="nomor" placeholder="Masukkan nomor HP Anda">
+                   <input type="text" class="form-control" id="nomor-listrik" name="nomor" placeholder="Masukkan nomor HP Anda">
                    <span id="operator-info"></span>
                   </div>
                 </div>
                 <div class="col-md-3">
                   <div class="form-group">
-                    <select id="nominal" class="form-control" name="nominal">
+                    <select id="nominal-listrik" class="form-control" name="nominal">
                     </select>
                   </div>
                 </div>
@@ -144,7 +144,7 @@
 
         </div>
       </div>
-    </div> -->
+    </div>
   </div>
 
 </div>
@@ -156,7 +156,7 @@
     $('#nomor-pulsa').on('input',function(e){
       var nomor = $('#nomor-pulsa').val();
 
-      if(nomor.length == 4){
+      if(nomor.length >= 4){
         var prefix = nomor.substring(0, 4);
         get_operator_pulsa(prefix);
       }
@@ -166,9 +166,18 @@
     $('#nomor-data').on('input',function(e){
       var nomor = $('#nomor-data').val();
 
-      if(nomor.length == 4){
+      if(nomor.length >= 4){
         var prefix = nomor.substring(0, 4);
         get_operator_data(prefix);
+      }
+    });
+
+    // nomor paket data
+    $('#nomor-listrik').on('input',function(e){
+      var nomor = $('#nomor-listrik').val();
+
+      if(nomor.length >= 1){
+        get_nominal_listrik('pln');
       }
     });
 
@@ -246,6 +255,29 @@
       });
     }
 
+    //listrik
+    function get_nominal_listrik(operator){
+      var type = $('#order_type_listrik').val();
+      $.ajax({
+          url: '<?=base_url('home/get_nominal/')?>' + type + "/" + operator,
+          type: 'POST',
+          dataType: 'JSON',
+          success: function(data){
+            console.log(data);
+            $.each(data, function(key, value) {
+              $.each(value, function(key, value) {
+                var nominal = formatRupiah(value.pulsa_nominal);
+                var price = formatRupiah(value.pulsa_price);
+                $('#nominal-listrik')
+                  .append($("<option></option>")
+                             .attr("value",value.pulsa_code)
+                             .html(nominal + " (Rp. " + price + ") "));
+              });
+            });
+          }
+      });
+    }
+
     //ppob-pulsa
     $("#ppob-pulsa").submit(function(e) {
         e.preventDefault(); // avoid to execute the actual submit of the form.
@@ -271,6 +303,29 @@
 
     //ppob-data
     $("#ppob-data").submit(function(e) {
+        e.preventDefault(); // avoid to execute the actual submit of the form.
+
+        var form = $(this);
+        var url = form.attr('action');
+
+        $.ajax({
+               type: "POST",
+               url: url,
+               data: form.serialize(), // serializes the form's elements.
+               dataType: "JSON",
+               success: function(data)
+               {
+                   console.log(data); // show response from the php script.
+                   $.each(data, function(key, value) {
+                      alert("status transaksi anda adalah " + value.message);
+                   });
+               }
+      });
+
+    });
+
+    //ppob-listrik
+    $("#ppob-listrik").submit(function(e) {
         e.preventDefault(); // avoid to execute the actual submit of the form.
 
         var form = $(this);
